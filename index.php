1b401e57d5f972ad60ccb0e6cf3e60cbaa850f25
boonruang <scubatoy@gmail.com>
2022-08-03 21:54:13 +0700

    22. Blog Listing Page

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ get_header(); ?>
     </div>
     
 <div class="container container--narrow page-section">
-      <?php while (have_posts()) {
+      <?php
+      while (have_posts()) {
           the_post(); ?>
         <div class="post-item">
             <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -30,7 +31,9 @@ get_header(); ?>
             </div>
         </div>
      <?php
-      } ?>
+      }
+      echo paginate_links();
+      ?>
 </div>    
 
 <?php get_footer();
