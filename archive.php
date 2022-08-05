27be9bf015b24b505eff9fa23e38ba83adee739c
boonruang <scubatoy@gmail.com>
2022-08-06 04:34:58 +0700

    46. Using Our pageBanner Function

--- archive.php
+++ archive.php
@@ -1,30 +1,11 @@
 <?php
 
-get_header(); ?>
-
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri(
-          'images/ocean.jpg'
-      ); ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            <?php the_archive_title(); ?>
-        
-            <!-- <?php
-            if (is_category()) {
-                single_cat_title();
-            }
-            if (is_author()) {
-                echo 'Posted by ';
-                the_author();
-            }
-            ?> -->
-        </h1>
-        <div class="page-banner__intro">
-          <p><?php the_archive_description(); ?></p>
-        </div>
-      </div>
-    </div>
+get_header();
+pageBanner(array(
+    'title' => get_the_archive_title(),
+    'subtitle' => get_the_archive_description()
+))
+?>
     
 <div class="container container--narrow page-section">
       <?php
@@ -36,7 +17,7 @@ get_header(); ?>
                 <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time(
      'n.j.y'
  ); ?> in <?php echo get_the_category_list(', '); ?></p>
-            </div>
+            < /div>
             <div class="generic-content">
                 <?php the_excerpt(); ?>
                 <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
