0e0f362633a992389765f27c871ae4f8563e746f
boonruang <scubatoy@gmail.com>
2022-08-31 03:38:42 +0700

    194. Challenge: Query Vars

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="page-banner">
+  <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri(
           'images/library-hero.jpg'
       ); ?>)"></div>
@@ -10,7 +10,7 @@
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
         <a href="<?php echo get_post_type_archive_link('program') ?>" class="btn btn--large btn--blue">Find Your Major</a>
       </div>
-    </div>
+  </div>
 
     <div class="full-width-split group">
       <div class="full-width-split__one">
--- page.php
+++ page.php
@@ -55,7 +55,23 @@ while (have_posts()) {
     ?>
 
       <div class="generic-content">
-        <?php the_content(); ?>
+        <?php the_content(); 
+
+           $skyColorValue = sanitize_text_field(get_query_var('skyColor'));
+           $grassColorValue = sanitize_text_field(get_query_var('grassColor'));
+           
+           if ($skyColorValue == 'blue' AND $grassColorValue == 'green') {
+              echo '<p>The Sky is blue today and the grass is green. Life is good.</p>';
+           }
+
+           ?>
+
+           <form method="GET">
+              <input name="skyColor" placeholder="Sky color">
+              <input name="grassColor" placeholder="Grass color">
+              <button>Submit</button>
+           </form>
+           
       </div>
     </div>
 
